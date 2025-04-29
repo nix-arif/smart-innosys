b2afd0dedda9507daf16204fee05985690414df9
nix@rif <arif@nixs-MacBook-Pro.local>
2025-04-29 09:00:43 +0800

    next.config.ts monorepo-plugin

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from "next";
+import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -7,6 +8,16 @@ const nextConfig: NextConfig = {
   },
   eslint: {
     ignoreDuringBuilds: true,
+  },
+  transpilePackages: ["@workspace/ui"],
+  webpack: (config, { isServer }) => {
+    // This is a workaround to avoid this Prisma issue on Vercel
+    // https://github.com/prisma/prisma/discussions/19499
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()];
+    }
+
+    return config;
   },
 };
 
