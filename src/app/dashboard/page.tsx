51eb2bc6ad07a14e130a982dc878851f1df94ab4
nix-arif <nix-arif@nixs-MacBook-Pro.local>
2025-05-02 15:01:39 +0800

    link

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,15 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 import React from "react";
 
 const DashboardPage = () => {
-  return <div>DashboardPage</div>;
+  return (
+    <div>
+      <Link href="/delivery-order">
+        <Button className="cursor-pointer">DELIVERY ORDER</Button>
+      </Link>
+    </div>
+  );
 };
 
 export default DashboardPage;
